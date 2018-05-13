e1a6cb26f8bd93d92dd4ed2900a2abe2c671858b
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-13 20:16:17 +0200

    React and Node, working together in dev mode

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const env = process.env.NODE_ENV || 'development';
 
 if (env === 'development') {
   process.env.NODE_ENV = 'development';
-  process.env.PORT = 3000;
+  process.env.PORT = 5000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/classapp';
 } else if (env === 'test') {
-  process.env.PORT = 3000;
+  process.env.PORT = 5000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/classapptest';
 }
